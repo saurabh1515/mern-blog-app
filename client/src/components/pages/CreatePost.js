6fc7934f2459430e93fba86929ada22d736bdef4
Saurabh Shukla <saurabh.shukla244@gmail.com>
2023-08-05 12:38:18 +0530

    intial commit

---
+++ client/src/components/pages/CreatePost.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import Editor from "../Editor";
+
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const createNewPost = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", files[0]);
+
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
+      body: data,
+      credentials: "include",
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  };
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+  return (
+    <form onSubmit={createNewPost}>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(event) => setSummary(event.target.value)}
+      />
+      <input type="file" onChange={(event) => setFiles(event.target.files)} />
+      <Editor value={content} onChange={setContent} />
+      <button style={{ margin: "5px" }}>Create post</button>
+    </form>
+  );
+};
+
+export default CreatePost;
